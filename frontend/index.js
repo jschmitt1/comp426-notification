29cd381aca41a5ada3a140b47e9f955573d2a688
willh18 <willh18@willh18-pc.attlocal.net>
2020-11-23 20:38:32 -0500

    Version 1.1.0

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,5 @@
-var express = require('express');
+
+/*var express = require('express');
 var app = express();
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,13 +14,49 @@ var server = app.listen(8081, function () {
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
+*/
 
 
 
 $(function() {
-    renderLiveNow();
-    renderOffline();
+    //renderLiveNow();
+    //renderOffline();
 });
+
+$( "#search-form" ).keyup(function(event){
+    let searchTerm = event.target.value;
+    autoCompleteName(searchTerm);
+});
+
+
+
+export async function autoCompleteName(search) {
+    let requestURL = 'http://3.88.71.149:3000/search';
+    let body = {
+        "query": search
+    };
+    const result = await axios({
+        method: 'post',
+        data: body,
+        url: requestURL
+    });
+    //console.log(result);
+    //console.log(result.data.body.data[0].display_name);
+
+    $(`#suggestions`).empty();  //empty out any items that might be left from last keypress
+    let nameList = result.data.body.data;
+    let suggestList = ``;
+    let len = (nameList.length > 5) ? 5 : nameList.length; //get at most 5 suggestions
+    //console.log(len);
+    for(let i = 0; i < len; i++){
+        suggestList += `<option value="${nameList[i].display_name}">`;
+    }
+    $(`#suggestions`).append(suggestList);
+}
+
+
+
+
 
 export async function renderLiveNow() {
     const $root = $(`#root`);
